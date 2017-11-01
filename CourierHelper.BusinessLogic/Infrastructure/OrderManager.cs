5b2ab546ebe8eb7511f26d0fa958d47763d90369
Pavlo Yerzhykevych <bizmonto@gmail.com>
2017-11-01 22:52:56 +0200

    Add orders managing core logic

---
+++ CourierHelper.BusinessLogic/Infrastructure/OrderManager.cs
@@ -0,0 +1,103 @@
+﻿using CourierHelper.BusinessLogic.DTO;
+using CourierHelper.BusinessLogic.DTO.Enums;
+using CourierHelper.BusinessLogic.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourierHelper.BusinessLogic.Infrastructure
+{
+	public class OrderManager
+	{
+		private MapboxService _mapBoxService;
+		private OrderService _orderService;
+		private CourierService _courierService;
+		private PointService _pointService;
+
+		private Queue<OrderDto> _ordersQueue;
+
+		public OrderManager()
+		{
+			_mapBoxService = new MapboxService("", MapBoxProfile.Driving);      //todo: get access token from appsettings
+			_orderService = new OrderService("");                               //todo: get connection string from appsettings
+			_courierService = new CourierService("");                           //todo: get connection string from appsettings
+			_pointService = new PointService("");                           //todo: get connection string from appsettings
+
+			_ordersQueue = new Queue<OrderDto>();
+		}
+
+		public void UpdateQueue()
+		{
+			var newOrders = _orderService.GetUnassignedOrders();
+			_ordersQueue.Concat(newOrders).Distinct();
+		}
+
+		public async Task ProceedNextOrder()
+		{
+			OrderDto order = _ordersQueue.Dequeue();
+			if(order == null)
+			{
+				return;
+			}
+
+			await _orderService.ChangeOrderStateAsync(order, OrderStateDto.Assignment);                 //2. Change order state to Assignment
+
+			PointDto orderLocationPoint = _pointService.GetOrderLocation(order.Id);                     //3. Get order destination point coordinates
+
+			IEnumerable<CourierDto> couriers = _courierService.GetNearestCouriers(orderLocationPoint);  //4. Find nearest couriers by next criterias (distance to point > number of assigned orders > courier state)
+
+			var possibleCandidate = couriers.FirstOrDefault(courier => courier.State == CourierStateDto.Idle);
+
+			if (possibleCandidate != null)
+			{
+				await _courierService.AssignOrder(possibleCandidate.Id, order.Id);
+				//todo: add recomended route
+			}
+			else
+			{
+				var bestRoute = await FindBestCandidateAsync(couriers, orderLocationPoint);
+
+				if (bestRoute == null)
+				{
+					_ordersQueue.Enqueue(order);
+					//todo: Can`t find best courier
+				}
+				else
+				{
+					await _courierService.AssignOrder(bestRoute.Courier.Id, order.Id);
+					//todo: update recomended route
+				}
+			}
+		}
+
+		private async Task<RouteCandidate> FindBestCandidateAsync(IEnumerable<CourierDto> couriers, PointDto orderPoint)
+		{
+			List<PointDto> pointsDto = new List<PointDto>();
+			SortedList<double, RouteCandidate> candidates = new SortedList<double, RouteCandidate>();
+
+			foreach (var courier in couriers)
+			{
+				PointDto courierLocation = _courierService.GetCourierLocation(courier.Id);
+				pointsDto.Add(courierLocation);
+				pointsDto.Add(orderPoint);
+
+				List<OrderDto> courierOrders = _orderService.GetCourierOrders(courier.Id);
+				foreach (var courierOrder in courierOrders)
+				{
+					var location = _pointService.GetOrderLocation(courierOrder.Id);
+					pointsDto.Add(location);
+				}
+
+				var optimizedPointsOrder = await _mapBoxService.OptimizeRouteAsync(pointsDto.ToArray());
+				var optimizedRoute = await _mapBoxService.BuildRouteAsync(optimizedPointsOrder);
+
+				RouteDto currentRoute = _courierService.GetCourierCurrentRoute(courier.Id);
+				candidates.Add(optimizedRoute.Distance - currentRoute.Distance, optimizedRoute);
+			}
+
+			return candidates.First().Value;
+		}
+	}
+}
--- CourierHelper.TaskManagerService/Service.cs
+++ CourierHelper.TaskManagerService/Service.cs
@@ -1,28 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Diagnostics;
-using System.Linq;
+﻿using CourierHelper.BusinessLogic.Infrastructure;
 using System.ServiceProcess;
-using System.Text;
-using System.Threading.Tasks;
+using System.Timers;
 
 namespace CourierHelper.TaskManagerService
 {
-    public partial class Service : ServiceBase
-    {
-        public Service()
-        {
-            InitializeComponent();
-        }
+	public partial class Service : ServiceBase
+	{
+		private Timer _ordersQueueTimer;
+		private Timer _ordersProcessingTimer;
+		private OrderManager _orderManager;
 
-        protected override void OnStart(string[] args)
-        {
-        }
+		public Service()
+		{
+			InitializeComponent();
+			_orderManager = new OrderManager();
+		}
 
-        protected override void OnStop()
-        {
-        }
-    }
+		protected override void OnStart(string[] args)
+		{
+			_ordersQueueTimer = new Timer();
+			_ordersQueueTimer.Interval = 5 * 60 * 1000;
+			_ordersQueueTimer.Elapsed += UpdateData;
+			_ordersQueueTimer.Enabled = true;
+
+			_ordersProcessingTimer = new Timer();
+			_ordersProcessingTimer.Interval = 5 * 60 * 1000;
+			_ordersProcessingTimer.Elapsed += ProceedData;
+			_ordersProcessingTimer.Enabled = true;
+		}
+
+		private void UpdateData(object sender, ElapsedEventArgs e)
+		{
+			_orderManager.UpdateQueue();
+		}
+		private void ProceedData(object sender, ElapsedEventArgs e)
+		{
+			_orderManager.ProceedNextOrder().Wait();
+		}
+
+		protected override void OnStop()
+		{
+			_ordersQueueTimer.Enabled = false;
+			_ordersProcessingTimer.Enabled = false;
+		}
+	}
 }
